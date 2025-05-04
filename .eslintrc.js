67e77787ca6bbefd7720fdd45a59c8e017b3e10e
DucThanhDao <daoducthanh0101@gmail.com>
2025-05-04 11:25:21 +0700

    chore: update eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,7 +15,9 @@ module.exports = {
   plugins: ['react'],
   rules: {
     'react/jsx-uses-react': 'off',
-    'react/react-in-jsx-scope': 'off'
+    'react/react-in-jsx-scope': 'off',
+    "react/no-unknown-property": ["error", { ignore: ["jsx", "js"] }],
+
   },
   settings: {
     react: {
